cab191196ee8362d7d32910f8a0c831a7cadbffd
make <make@makedeiMac.local>
2018-05-10 16:16:32 +0800

    first commit

---
+++ LineDemo/MKCallOutView.swift
@@ -0,0 +1,21 @@
+//
+//  MKCallOutView.swift
+//  LineDemo
+//
+//  Created by make on 2018/5/10.
+//  Copyright © 2018年 make. All rights reserved.
+//
+
+import UIKit
+
+class MKCallOutView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
